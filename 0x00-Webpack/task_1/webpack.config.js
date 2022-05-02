2a3da89384b7aad857e2eecfb9a7e6464564002b
resiato <resiatolaitete@gmail.com>
2022-05-02 16:42:07 +0000

    [Fix] Use Eslint to fix code

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -3,11 +3,11 @@ const path = require('path');
 module.exports = {
   entry: {
   main: path.resolve(__dirname, './js/dashboard_main.js'),
-},
+  },
   output: {
-  filename: 'bundle.js',
-  path: path.resolve(__dirname, 'public'),
-},
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
   mode: 'production',
 };
   
