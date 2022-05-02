0d0c57dea629c6379d3c00e52171462941e85952
resiato <resiatolaitete@gmail.com>
2022-05-02 17:05:47 +0000

    [Fix] Use ESlint

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -7,7 +7,9 @@ $('body').append('<button>Click here to get started</button>');
 $('body').append("<p id='count'></p>");
 $('body').append('<p>Copyright - Holberton School</p>');
 
+
 let count = 0;
+
 function updateCounter() {
   count += 1;
   $('#count').text('${count} clicks on the button');
