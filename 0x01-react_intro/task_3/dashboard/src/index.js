08a2550d634ff4978d0057613f53aa00b2ee3c46
resiato <resiatolaitete@gmail.com>
2022-05-13 18:45:36 +0300

    Add tests

---
+++ 0x01-react_intro/task_3/dashboard/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import App from './App';
+import ReactDOM from 'react-dom';
+import Notifications from './Notifications';
+import reportWebVitals from './reportWebVitals';
+
+//create a root.
+ReactDOM.render(
+  <React.StrictMode>
+    <div className='root-notifications'>
+      <Notifications />
+    </div>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
