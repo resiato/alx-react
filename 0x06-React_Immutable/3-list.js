bd244226e2ebd8d922ddc5cbf22ee66a51b75e90
resiato <resiatolaitete@gmail.com>
2022-05-26 22:47:42 +0300

    Add task

--- 0x06-React_Immutable/3-list.js
+++ 0x06-React_Immutable/3-list.js
@@ -1,14 +1,8 @@
-import { Map } from "immutable";
+import { List } from "immutable";
 
-export const map = Map({
-  1: "Liam",
-  2: "Noah",
-  3: "Elijah",
-  4: "Oliver",
-  5: "Jacob",
-  6: "Lucas",
-});
-
-export const map2 = map.withMutations((mapItem) => {
-  mapItem.set(2, "Benjamin").set(4, "Oliver");
-});
+export function getListObject(array) {
+  return List(array);
+}
+export function addElementToList(list, element) {
+  return list.push(element);
+}
